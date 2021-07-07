ed3ce8fe853cbb4a424efa5da8c71c7f9ac3df7f
MS7700 <50929878+MS7700@users.noreply.github.com>
2021-07-07 16:58:02 -0400

    Minor changes
    Se agregó la conexión y las entidades.

---
+++ PracticaIII/CreacionXML/CreacionXML/src/conexion/PrestamoDB.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import entity.Estudiante;
+import entity.Pago;
+import entity.Prestamo;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author mslopez
+ */
+public class PrestamoDB {
+    
+    public List<Prestamo> getPrestamosDB(){
+        List<Prestamo> prestamos = new ArrayList<Prestamo>();
+        
+        return prestamos;
+    }
+    
+    private Prestamo getPrestamoDB(int id){
+        return null;
+    }
+    
+    private Estudiante getEstudiante(int id){
+        return null;
+    }
+    
+    private Pago getPago(int id){
+        return null;
+    }
+}
