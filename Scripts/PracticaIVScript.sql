7f1023a257ecbd6ce264660b2179c7942e5b6387
MS7700 <50929878+MS7700@users.noreply.github.com>
2021-07-08 12:35:07 -0400

    Script PracticaIV añadido
    Script PracticaIV añadido

---
+++ Scripts/PracticaIVScript.sql
@@ -0,0 +1,13 @@
+create table PagosFundapec (
+matricula varchar(12),
+MontoPagado decimal
+)
+
+insert into PagosFundapec values('20190154',33000);
+insert into PagosFundapec values('20190154',44000);
+insert into PagosFundapec values('20190041',52000);
+insert into PagosFundapec values('20190041',42000);
+insert into PagosFundapec values('20170234',20000);
+insert into PagosFundapec values('20170234',22000);
+insert into PagosFundapec values('20183167',15000);
+insert into PagosFundapec values('20183167',18000);
