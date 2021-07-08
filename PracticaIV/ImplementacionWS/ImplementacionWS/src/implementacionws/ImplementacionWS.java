b531ef823c1a563e09d8197e321a6c91a6327a20
MS7700 <50929878+MS7700@users.noreply.github.com>
2021-07-08 14:24:37 -0400

    Added Implementacion WS
    Se creó el proyecto de implementación de WS

---
+++ PracticaIV/ImplementacionWS/ImplementacionWS/src/implementacionws/ImplementacionWS.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package implementacionws;
+
+/**
+ *
+ * @author mslopez
+ */
+public class ImplementacionWS {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+
+    public ImplementacionWS() {
+    }
+    
+    //Modificar esto
+    public double getMontoFromMatricula(String matricula){
+        double monto = 1000; //Sustituir por web service
+        //double monto = sumarMontosMatricula(matricula)
+        return monto;
+    }
+    
+}
